897f93debd0f6882a9024ecc11feab79eec50352
evaughn@nd.edu <evaughn@nd.edu@fefebba2-aa7b-c57e-c3e4-dd0fed8b5c13>
2013-04-29 00:30:55 +0000

    + Added the first version of the CUDA market averager.

--- SACudaLibrary/src/SACudaKernel.cu
+++ SACudaLibrary/src/SACudaKernel.cu
@@ -7,6 +7,17 @@ __global__ void FindInverseTrendsKernel(double * data, int length)
 
 __global__ void CalculateMarketAverageKernel(double * data, int entries, int timesteps)
 {
+	int ordinal = threadIdx.x + blockIdx.x*blockDim.x - 1;
+	double sum = 0;
 
+	if(ordinal >= timesteps)
+		return;
+
+	for(int i = 0; i < entries; ++i)
+	{
+		sum += data[i*timesteps + ordinal];
+	}
+
+	data[ordinal] = sum/entries;
 }
 
