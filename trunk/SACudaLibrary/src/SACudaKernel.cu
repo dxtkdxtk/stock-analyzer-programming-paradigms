f28b6a4e2f4a36946bf42e163395ac3a41ca669f
evaughn@nd.edu <evaughn@nd.edu@fefebba2-aa7b-c57e-c3e4-dd0fed8b5c13>
2013-05-03 04:31:48 +0000

    + New CUDA Market Average Kernel

--- trunk/SACudaLibrary/src/SACudaKernel.cu
+++ trunk/SACudaLibrary/src/SACudaKernel.cu
@@ -5,9 +5,10 @@ __global__ void FindInverseTrendsKernel(double * data, int length)
 	
 }
 
-#define GETDATA(e, t) ((e<entries)?(e*timesteps + t):0)
+#define DATAINDEX(e, t) ((e<entries)?(e*timesteps + t):-1)
+#define GETDATA(e, t) ((e<entries)?data[DATAINDEX(e,t)]:0)
 
-__global__ void CalculateMarketAverageKernel(double * data, int entries, int timesteps)
+__global__ void CalculateMarketAverageKernelReduce(double * data, int entries, int timesteps)
 {
 	int entryid = (threadIdx.x + blockIdx.x*blockDim.x);
 	int timestepid = (threadIdx.y + blockIdx.y*blockDim.y);
@@ -15,7 +16,16 @@ __global__ void CalculateMarketAverageKernel(double * data, int entries, int tim
 	if(entryid*2 >= entries || timestepid >= timesteps)
 		return;
 
-	data[GETDATA(entryid, timestepid)] += data[GETDATA(((entries-1)/2 + 1 + entryid), timestepid)];
-	data[GETDATA(entryid, timestepid)] /= 2;
+	data[DATAINDEX(entryid, timestepid)] += GETDATA(((entries-1)/2 + 1 + entryid), timestepid);
+}
+
+__global__ void CalculateMarketAverageKernelFinalize(double * data, int entries, int timesteps)
+{
+	int timestepid = (threadIdx.x + blockIdx.x*blockDim.x);
+
+	if(timestepid >= timesteps)
+		return;
+
+	data[timestepid] /= entries;
 }
 
--- trunk/SACudaLibrary/src/SACudaKernel.h
+++ trunk/SACudaLibrary/src/SACudaKernel.h
@@ -2,7 +2,8 @@
 #define SACUDAKERNEL_H
 
 __global__ void FindInverseTrendsKernel(double * data, int length);
-__global__ void CalculateMarketAverageKernel(double * data, int entries, int timesteps);
+__global__ void CalculateMarketAverageKernelReduce(double * data, int entries, int timesteps);
+__global__ void CalculateMarketAverageKernelFinalize(double * data, int entries, int timesteps);
 
 #endif
 
